992e631680125413b2b4e3811d01db51ca03b600
aurorami1211 <59692969+aurorami1211@users.noreply.github.com>
2020-02-12 18:00:08 +0100

    Update queue.c

--- skeleton_project/source/queue.c
+++ skeleton_project/source/queue.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "queue.h"
+#include "hardware.h"
+
+
 
 void updateQueue(){
-    for (int i = 0; i < HARDWARE_NUMBER_OF_FLOORS; ++i){
-        for (HardwareOrder j = HARDWARE_ORDER_UP; j < HARDWARE_ORDER_DOWN; ++j){ //Hardwareorders
-            QueueMatrix[i][j] = hardware_read_order(i + 1, j);
+	int i;
+	HardwareOrder j; 
+
+    for (i = 0; i < HARDWARE_NUMBER_OF_FLOORS; ++i){
+        for (j = HARDWARE_ORDER_UP; j <= HARDWARE_ORDER_DOWN; ++j){ //Hardwareorders
+        	if (hardware_read_order(i, j)){
+        		//int a = i+1;
+        		printf("%d",i);
+        	}
+        	Q_MATRIX[i][0] = hardware_read_order(i, j);
         }
     }
 }
