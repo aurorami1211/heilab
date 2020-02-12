8c75774b91f65e6d7cd4ae99c144c21828ddce6f
aurorami1211 <59692969+aurorami1211@users.noreply.github.com>
2020-02-12 11:38:55 +0100

    updated door_open

--- skeleton_project/source/door.c
+++ skeleton_project/source/door.c
@@ -1,12 +1,14 @@
 void open_door(){
-do {
-  int time = 3000;
-  clock_t start_time = clock();
+  do {
+    int time = 3000;
+    clock_t start_time = clock();
+
+    while(clock() < start_time + time) {
+      hardware_command_door_open(1);
+      break;
+    } 
+  } while (!hardware_read_obstruction);
   
-  while(clock() < start_time + time) {
-    hardware_command_door_open(1);
-  }
-  
+  hardware_command_door_open(0);
 }
-  while (!hardware_read_obstruction);
-}
+
