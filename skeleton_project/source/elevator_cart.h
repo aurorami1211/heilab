04c062ca9b57bdc8ab1076baa7f889176fa44e52
aurorami1211 <59692969+aurorami1211@users.noreply.github.com>
2020-02-12 17:59:45 +0100

    Update elevator_cart.h

--- skeleton_project/source/elevator_cart.h
+++ skeleton_project/source/elevator_cart.h
@@ -1,9 +1,10 @@
- 
 #include <stdio.h>
 #include <stdlib.h>
 #include "hardware.h"
-#include "elevator_cart.h"
+//#include <time.h>
 
 int get_floor_number();
 
 void start_elevator();
+
+void door_open();
