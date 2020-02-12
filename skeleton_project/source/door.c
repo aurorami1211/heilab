38a6676cb5488371fa621b9282824943f8f422d2
aurorami1211 <59692969+aurorami1211@users.noreply.github.com>
2020-02-12 11:34:04 +0100

    added door_open()

---
+++ skeleton_project/source/door.c
@@ -0,0 +1,12 @@
+void open_door(){
+do {
+  int time = 3000;
+  clock_t start_time = clock();
+  
+  while(clock() < start_time + time) {
+    hardware_command_door_open(1);
+  }
+  
+}
+  while (!hardware_read_obstruction);
+}
