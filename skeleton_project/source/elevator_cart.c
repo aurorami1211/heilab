00edf35fbcb917873dbe794e4451eb0b2e3e5e51
aurorami1211 <59692969+aurorami1211@users.noreply.github.com>
2020-02-12 11:00:54 +0100

    added start_elevator() and get_floor_number()

--- skeleton_project/source/elevator_cart.c
+++ skeleton_project/source/elevator_cart.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hardware.h"
+#include "elevator_cart.h"
+
+
+int get_floor_number(){
+  if (hardware_read_floor_sensor(0))
+      return 1;
+  else if (hardware_read_floor_sensor(1))
+      return 2;
+  else if (hardware_read_floor_sensor(2))
+      return 3;
+  else if (hardware_read_floor_sensor(3))
+      return 4;
+}
+
+void start_elevator(){
+   int error = hardware_init();
+   if(error != 0){
+        fprintf(stderr, "Unable to initialize hardware\n");
+        exit(1);
+    }
+  
+  //assert(get_floor_number() =< 0);
+  //assert(get_floor_number() > HARDWARE_NUMBER_OF FLOORS); //Hvis heisen er mellom 1 og 4 så vil den ikke gå videre. 
+  
+  while (get_floor_number() =< 0 && get_floor_number() > HARDWARE_NUMBER_OF FLOORS){
+      hardware_command_movemnet(HARDWARE_MOVEMENT_UP);
+  }
+}
+
+
+
 
