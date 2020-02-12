5b19570aa59e194c4ddb26f2de1b55fda3e75bea
sofietrovik <59829095+sofietrovik@users.noreply.github.com>
2020-02-12 16:26:20 +0100

    Update queue.h

---
+++ skeleton_project/source/queue.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hardware.h"
+
+#DEFINE 
+
+int width = 5;
+int height = 5;
+int** queueMatrix = malloc int*[width];
+for(int i = 0; i < width; ++i){
+   queueMatrix[i] = malloc int[height];
+}
+
+class Queue{
+    private:
+        int** queueMatrix;
+    public:
+        void updateQueue()
+        Queue();
+    
+}
