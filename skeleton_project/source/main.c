68f982fd4c207737e1e22a81f576df7f40f19dd7
aurorami1211 <59692969+aurorami1211@users.noreply.github.com>
2020-02-12 18:02:29 +0100

    Update main.c

--- skeleton_project/source/main.c
+++ skeleton_project/source/main.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "hardware.h"
+#include "elevator_cart.h"
+#include "queue.h"
+
+
 
 int main(){
-    int error = hardware_init();
-    if(error != 0){
-        fprintf(stderr, "Unable to initialize hardware\n");
-        exit(1);
-    }
-
-    printf("=== Example Program ===\n");
     printf("Press the stop button on the elevator panel to exit\n");
 
     hardware_command_movement(HARDWARE_MOVEMENT_UP);
 
+    start_elevator();
+
     while(1){
+        
         if(hardware_read_stop_signal()){
             hardware_command_movement(HARDWARE_MOVEMENT_STOP);
             break;
@@ -26,5 +26,6 @@ int main(){
         if(hardware_read_floor_sensor(HARDWARE_NUMBER_OF_FLOORS - 1)){
             hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
         }
+        updateQueue();
     }
 }
